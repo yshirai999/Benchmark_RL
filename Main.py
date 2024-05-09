f70822eb5164368c5960e34cfacaad41cb92b691
yshirai999 <yshirai@uw.edu>
2024-05-09 11:42:24 -0700

    v

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 ##########################################
 ### To run this file: & PYTHONPATH PATH
+### PYTHONPATH is path to python in conda
+### PATH is this python file path
 ##########################################
 ### Libraries
 ##########################################
@@ -19,7 +21,6 @@ import os
 ##########################################
 
 Benv = BenchmarkReplication(W = 1, N = 10, Nsim = 100, Dynamics = 'BS', start_time = 0, T = 26, dT = 1, r = 0, mu = [0.03,0.01], sigma = [0.3, 0.2])
-#check_env(Benv) #check environment is accepted by SB3
 Benv.seed(seed=random.seed(10))
 
 steps = 100000
@@ -31,10 +32,10 @@ except:
     print("Training model...")
     model = PPO('MlpPolicy', DummyVecEnv([lambda: Benv]), learning_rate=0.001, verbose=1)
     model.learn(total_timesteps=steps)
-    path = f"C:/Users/yoshi/OneDrive/Desktop/Research/Benchmark_RL/BS_PPO_Models"
+    path = f"C:/Users/yoshi/OneDrive/Desktop/Research/Benchmark_RL/BS_PPO_Models" # PATH to the BS_PPO_Models folder
     if not os.path.exists(path):
         os.makedirs(path)
-    path = f"C:/Users/yoshi/OneDrive/Desktop/Research/Benchmark_RL/BS_PPO_Models/BS_PPO_{str(steps)}"
+    path = f"C:/Users/yoshi/OneDrive/Desktop/Research/Benchmark_RL/BS_PPO_Models/BS_PPO_{str(steps)}" # PATH to the model
     model.save(path)
 
 ##########################################
