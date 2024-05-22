99290ead27a8016febc1a8babe5c0e8a5e079465
yshirai999 <yshirai@uw.edu>
2024-05-22 08:32:16 -0700

    protobufdowngraded

--- Main.py
+++ Main.py
@@ -37,7 +37,7 @@ Benv = BenchmarkReplication(W = W, N = N, Nsim = Nsim,
                                T = T, dT = dT, r = r, mu = mu, sigma = sigma)
 Benv.seed(seed=random.seed(10))
 
-steps = 2000
+steps = 10000
 
 path_folder = f"C:/Users/yoshi/OneDrive/Desktop/Research/Benchmark_RL/BS_PPO" # PATH to the BS_PPO_Models folder
 path = f"{path_folder}/BS_PPO_{str(steps)}_{str(int(sigma[0]*100))}{str(int(sigma[1]*100))}"
